01d5077a50f21e18edf3cada31dc9d38953d7d7a
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-10 12:57:15 +0530

    create SecurityConfig and create @Bean to get UserdetailsService.

---
+++ src/main/java/com/cms/cms/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.cms.cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin= User.withUsername("admin1")
+                .password("Pwd1")
+                .roles("ADMIN","USER")
+                .build();
+
+        UserDetails user= User.withUsername("user1")
+                .password("Pwd1")
+                .roles("USER")
+                .build();
+
+    }
+
+    @Bean
+
+}
