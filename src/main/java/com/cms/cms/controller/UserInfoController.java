0b271e9c5c5ee3ed22b65ee3bc8f3d191f48291b
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-10 19:04:47 +0530

    Authentication manager revised and use in th the UserInfo controller

--- src/main/java/com/cms/cms/controller/UserInfoController.java
+++ src/main/java/com/cms/cms/controller/UserInfoController.java
@@ -6,6 +6,9 @@ import com.cms.cms.servise.UserInfoService;
 import com.cms.cms.servise.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +34,13 @@ public class UserInfoController {
 
     @PostMapping("/authenticate")
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest){
-        return jwtService.generateToken(authRequest.getUsername());
+        Authentication authentication =authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(),authRequest.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(authRequest.getUsername());
+        }else {
+            throw new UsernameNotFoundException("Invalid user request !...");
+        }
     }
 }
--- src/main/java/com/cms/cms/servise/security/JwtService.java
+++ src/main/java/com/cms/cms/servise/security/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Component;
 
@@ -42,8 +43,7 @@ public class JwtService {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
