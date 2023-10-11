3379cc69be5d4313e3b31cc0785faa2369f1df8e
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-11 21:09:14 +0530

    createRefreshToken method created in refresh token service

--- src/main/java/com/cms/cms/servise/RefreshTokenService.java
+++ src/main/java/com/cms/cms/servise/RefreshTokenService.java
@@ -1,9 +1,13 @@
 package com.cms.cms.servise;
 
+import com.cms.cms.dao.RefreshToken;
 import com.cms.cms.repository.RefreshTokenRepository;
 import com.cms.cms.repository.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class RefreshTokenService {
@@ -13,4 +17,14 @@ public class RefreshTokenService {
 
     @Autowired
     private UserInfoRepository userInfoRepository;
+
+    public RefreshToken createRefreshToken(String username){
+       RefreshToken refreshToken= RefreshToken.builder()
+                .userInfo(userInfoRepository.findByUsername(username).get())
+                .token(UUID.randomUUID().toString())
+                .expiryDate(Instant.now().plusMillis(60000)) //10 min
+                .build();
+
+       return refreshTokenRepository.save(refreshToken);
+    }
 }
