eea3dc50113a2062ac8fbb78b59b852ae27e802d
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-11 21:39:51 +0530

    findByToken and verifyExpiration methode implement in RefreshToken service classs

--- src/main/java/com/cms/cms/repository/RefreshTokenRepository.java
+++ src/main/java/com/cms/cms/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.cms.cms.repository;
 import com.cms.cms.dao.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Integer> {
+    Optional<RefreshToken> findByToken(String token);
 }
--- src/main/java/com/cms/cms/servise/RefreshTokenService.java
+++ src/main/java/com/cms/cms/servise/RefreshTokenService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,5 +27,17 @@ public class RefreshTokenService {
                 .build();
 
        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public Optional<RefreshToken> findByToken(String token){
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token){
+        if(token.getExpiryDate().compareTo(Instant.now())<0){
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException(token.getToken()+ " Refresh token was expired ");
+        }
+        return token;
     }
 }
